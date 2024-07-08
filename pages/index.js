f5ff01f2a725f5d067867641b934df5fa70d8989
Lucas Dias <154710695+Lucas89408@users.noreply.github.com>
2024-07-08 18:32:36 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Scheila, i love you. If you love me, laugh a little</h1>
+  return <h1>Scheila, i love you so much. If you love me, laugh a little</h1>
 }
 
 export default Home;

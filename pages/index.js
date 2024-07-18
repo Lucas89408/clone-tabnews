95a11690d4ca39b529ac0dcc4fefaa33ac209090
Lucas Dias <154710695+Lucas89408@users.noreply.github.com>
2024-07-18 17:56:37 +0000

    Adicona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
 function Home() {
-  return <h1>Oii pessoa linda, passando rapidinho para te lembrar que, independente de qualquer coisa, Eu Te Amo.</h1>
+  return (
+    <h1>
+      Oii pessoa linda, passando rapidinho para te lembrar que, independente de
+      qualquer coisa, Eu Te Amo.
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("identacao errada");
 }
 
 export default Home;
